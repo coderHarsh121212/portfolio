731b821065fe0554fdf6f30c2836fa5727a4ba4b
coderHarsh121212 <codesource287@gmail.com>
2024-02-12 22:27:25 +0530

    change
    Signed-off-by: coderHarsh121212 <codesource287@gmail.com>

--- src/components/PortfolioProject.js
+++ src/components/PortfolioProject.js
@@ -8,20 +8,17 @@ const PortfolioProject = () => {
   return (
     <div className="bg-[rgba(0.13,0.13,0.13,1)]  text-white" id="projects">
       <div className="pt-24">
-        <h1 className="font-serif text-6xl text-center hover-type">Projects</h1>
+        <h1 className="font-serif text-6xl text-center hover-type" >Projects</h1>
         <div className="portfolio-container h-full bg-black">
-          <div
-            className="w-full p-5 font-serif my-5 md:grid md:grid-cols-2 md:gap-x-10 md:items-center"
-            data-aos="fade-right"
-          >
-            <div className="w-full md:1/2">
+          <div className="w-full p-5 font-serif my-5 md:grid md:grid-cols-2 md:gap-x-10 md:items-center" data-aos="fade-right">
+            <div className="w-full md:1/2" >
               <img
                 src="https://www.cryptoryancy.com/wp-content/uploads/2020/12/load-image-2.jpeg"
                 className="w-full"
                 alt="1"
               ></img>
             </div>
-            <div className="mt-2 md:w-4/5">
+            <div className="mt-2 md:w-4/5" >
               <h1 className="font-medium text-2xl text-center">Crypto Wiki</h1>
               <p className="my-4">
                 This is a Advance Crypto Responsive website.Rather than just
@@ -31,23 +28,13 @@ const PortfolioProject = () => {
                 all cryptoCurrency.
               </p>
               <div className="flex gap-7 px-10 py-3  justify-center md:justify-start md:text-sm md:py-0">
-                <a
-                  className="btn-css md:p-1 py-2"
-                  href="https://github.com/coderHarsh121212/Coin-dash.git"
-                >
-                  Source Code
-                </a>
-                <a className="btn-css md:p-1 py-2" href="cyrpto-dasher.netlify.app/">
-                  Live Demo
-                </a>
+                <a className="btn-css md:p-1" href="https://github.com/coderHarsh121212/Coin-dash.git">Source Code</a>
+                <a className="btn-css md:p-1" href="cyrpto-dasher.netlify.app/">Live Demo</a>
               </div>
             </div>
           </div>
-          <div
-            className="w-full p-5 font-serif my-5 md:grid md:grid-cols-2 md:gap-x-10 md:items-center  flex flex-col-reverse"
-            data-aos="fade-right"
-          >
-            <div className="mt-2 md:w-4/5">
+          <div className="w-full p-5 font-serif my-5 md:grid md:grid-cols-2 md:gap-x-10 md:items-center  flex flex-col-reverse"  data-aos="fade-right">
+            <div className="mt-2 md:w-4/5" >
               <h1 className="font-medium text-2xl text-center">Blood Dev</h1>
               <p className="my-4">
                 This is a website which is based on a real-life based problem.It
@@ -56,22 +43,12 @@ const PortfolioProject = () => {
                 feature which basically tell about the nearest or the other
                 blood banks or camps address.
               </p>
-              <div className="flex gap-7 px-10 py-3  justify-center md:justify-start md:text-sm md:py-0">
-                <a
-                  className="btn-css md:p-1 py-2"
-                  href="https://github.com/coderHarsh121212/blood-bank.git"
-                >
-                  Source Code
-                </a>
-                <a
-                  className="btn-css md:p-1 py-2"
-                  href="bloodbank-cloud.netlify.app/"
-                >
-                  Live Demo
-                </a>
+              <div className="flex gap-7 px-10 py-3  justify-center md:justify-start md:text-sm md:py-0" >
+                <a className="btn-css md:p-1" href="https://github.com/coderHarsh121212/blood-bank.git">Source Code</a>
+                <a className="btn-css md:p-1" href="bloodbank-cloud.netlify.app/">Live Demo</a>
               </div>
             </div>
-            <div className="w-full md:1/2">
+            <div className="w-full md:1/2" >
               <img
                 src="https://wallpapercave.com/wp/wp7898131.jpg"
                 className="w-full"
@@ -79,10 +56,7 @@ const PortfolioProject = () => {
               ></img>
             </div>
           </div>
-          <div
-            className="w-full p-5 font-serif my-5 md:grid md:grid-cols-2 md:gap-x-10 md:items-center flex flex-col"
-            data-aos="fade-right"
-          >
+          <div className="w-full p-5 font-serif my-5 md:grid md:grid-cols-2 md:gap-x-10 md:items-center flex flex-col" data-aos="fade-right" >
             <div className="w-full md:1/2">
               <img
                 src="https://th.bing.com/th/id/OIP.dTjMc-Pb3pQvwrhU359jnwHaE8?rs=1&pid=ImgDetMain"
@@ -104,9 +78,7 @@ const PortfolioProject = () => {
                 source code
               </p>
               <div className="flex gap-7 px-10 py-3  justify-center md:justify-start md:text-sm md:py-0">
-                <a className="btn-css md:p-1 py-2" href="codesource.netlify.app/">
-                  Live Demo
-                </a>
+                <a className="btn-css md:p-1" href="codesource.netlify.app/">Live Demo</a>
               </div>
             </div>
           </div>
