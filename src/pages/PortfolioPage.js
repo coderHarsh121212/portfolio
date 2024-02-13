aa2c13d71b7532a584d9cb12f3c9ab7e4d760162
coderHarsh121212 <codesource287@gmail.com>
2024-02-13 12:20:46 +0530

    change
    Signed-off-by: coderHarsh121212 <codesource287@gmail.com>

--- src/pages/PortfolioPage.js
+++ src/pages/PortfolioPage.js
@@ -1,19 +1,18 @@
-import React from 'react'
-import AboutPage from '../components/AboutPage'
-import Contact from '../components/Contact'
-import HomePage from '../components/HomePage'
-import PortfolioProject from '../components/PortfolioProject'
+import React from "react";
+import AboutPage from "../components/AboutPage";
+import Contact from "../components/Contact";
+import HomePage from "../components/HomePage";
+import PortfolioProject from "../components/PortfolioProject";
 
 const PortfolioPage = () => {
   return (
-    <div className=' m-0'>
-        <HomePage/>
-         <AboutPage/>
-         <PortfolioProject/>
-        <Contact/> 
+    <div className=" m-0">
+      <HomePage />
+      <AboutPage />
+      <PortfolioProject />
+      <Contact />
     </div>
-    
-  )
-}
+  );
+};
 
-export default PortfolioPage
+export default PortfolioPage;
